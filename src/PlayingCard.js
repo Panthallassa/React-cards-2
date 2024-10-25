76552a416586e74f2e027652285a4d95296f3bbd
Ana Grieser <anag@Anas-MacBook-Pro.local>
2024-10-25 15:20:12 -0600

    Finished step 2

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
+import useFlip from "./hooks";
 import backOfCard from "./back.png";
-import "./PlayingCard.css"
+import "./PlayingCard.css";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
-  return (
-    <img
-      src={isFacingUp ? front : back}
-      alt="playing card"
-      onClick={flipCard}
-      className="PlayingCard Card"
-    />
-  );
+	// const [isFacingUp, setIsFacingUp] = useState(true);
+	// const flipCard = () => {
+	//   setIsFacingUp(isUp => !isUp);
+	// };
+	// Replaced with:
+	const [isFacingUp, flipCard] = useFlip();
+
+	return (
+		<img
+			src={isFacingUp ? front : back}
+			alt='playing card'
+			onClick={flipCard}
+			className='PlayingCard Card'
+		/>
+	);
 }
 
 export default PlayingCard;
---
+++ src/hooks.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function useFlip() {
+	const [isFacingUp, setIsFacingUp] = useState(true);
+
+	const flipCard = () => {
+		setIsFacingUp((isUp) => !isUp);
+	};
+
+	return [isFacingUp, flipCard];
+}
+
+export default useFlip;
